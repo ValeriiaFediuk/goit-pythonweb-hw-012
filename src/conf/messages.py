ac854e1c5e5dcc0e7b083a906a096757f2cce2a7
ValeriiaFediuk <fediuk.valeriia14@gmail.com>
2025-02-08 20:52:09 +0100

    add test auth

--- src/conf/messages.py
+++ src/conf/messages.py
@@ -6,3 +6,9 @@ DATABASE_ERROR_CONNECT_MESSAGE = "Error connecting to the database"
 DATABASE_SESSION_NOT_INITIALIZED = "Database session is not initialized"
 INVALID_BIRTHDAY = "Birthday cannot be in the future"
 INVALID_PHONE_NUMBER = "Phone number must contain only digits"
+
+API_ERROR_USER_ALREADY_EXIST = "Користувач з таким email вже існує"
+API_ERROR_USER_NOT_AUTHORIZED = "Електронна адреса не підтверджена"
+API_ERROR_WRONG_PASSWORD = "Неправильний логін або пароль"
+
+API_EMAIL_CONFIRMED = "Ваша електронна пошта вже підтверджена"
